e0077ad2fd8d08627ca504c112b5e0dbb20ab6b9
Mattia Pepin Taiana <mattia.taiana@gmail.com>
2023-02-07 20:38:52 +0100

    start experimenting with axum

--- displayer/src/main.rs
+++ displayer/src/main.rs
@@ -17,94 +17,55 @@
 #[macro_use]
 extern crate lazy_static;
 
-use tera::{Context, Tera};
 use std::{
     net::{TcpListener, TcpStream},
     io::{prelude::*, BufReader, Read},
     fs::File
 };
-use image::{GenericImageView, ImageFormat};
+use askama::Template;
+use askama_axum::IntoResponse;
+use axum::{
+    extract, 
+    http::StatusCode, 
+    response::{Html, Response}, 
+    routing::get, 
+    Router,
+    Extension,
+};
+use std::{
+    sync::Arc,
+    net::SocketAddr,
+};
+// use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use log::{info, warn};
 
 
 /* SETTINGS */
 const PORT: i32 = 8000;
 
 
-lazy_static! {
-    pub static ref TEMPLATES: Tera = {
-        let tera = match Tera::new("src/templates/**/*.html") {
-            Ok(t) => t,
-            Err(e) => {
-                println!("Parsing error(s): {}", e);
-                ::std::process::exit(1);
-            }
-        };
-        tera
-    };
+#[derive(Template)]
+#[template(path = "index.html")]
+struct TemplateInformation<'a> {
+    test: &'a String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let t = TemplateInformation{ test: &"Mattia".to_string() };
 
-    let mut context = Context::new();
-/* example:
-    let name = "Mattia";
-    context.insert("name", &name);
-*/
-    let contents = match TEMPLATES.render("index.html", &context) {
-        Ok(s) => s,
-        Err(e) => {
-            println!("Error: {}", e);
-            ::std::process::exit(1);
-        }
-    };
+    let app = Router::new()
+        .route("/", get(root));
 
-
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", PORT)).unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream, &contents);
-    }
+    axum::Server::bind(&format!("0.0.0.0:{}", PORT).parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    info!("Listening on : localhost:{}", PORT);
 }
 
-
-fn handle_connection(mut stream: TcpStream, contents: &String) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    let errorString = "404 Not Found".to_string();
-    let mut string = String::new();
-
-    if http_request.len() == 0 {
-        let response = format!("HTTP/1.1 200 OK\r\nContentLength: 0\r\n\r\n{}", contents);
-        stream.write_all(response.as_bytes()).unwrap();
-        return;
-    }
-    let (status_line, response_body) = match http_request[0].split_whitespace().nth(1).unwrap() {
-        "/" => ("HTTP/1.1 200 OK", contents),
-        "/assets/image" => {
-            let mut buf = Vec::new();
-            let mut file = File::open("src/assets/IMG_9147.JPG").unwrap();
-            file.read_to_end(&mut buf).unwrap();
-            let image = image::load_from_memory(&buf).unwrap();
-            let format = ImageFormat::JPEG;
-
-            /**/
-            ("HTTP/1.1 200 OK", &string)
-        },
-        _ => ("HTTP/1.1 404 Not Found", &errorString),
-    };
-
-    let length = response_body.len();
-    let response = format!("HTTP/1.1 200 OK\r\nContentLength: {}\r\n\r\n{}", length, response_body);
-
-    // println!("Request: {:#?}", http_request);
-    // println!("Response: {:#?}", response);
-
-    stream.write_all(response.as_bytes()).unwrap();
+async fn root(
+    Extension(templateInformation): Extension<TemplateInformation<'_>>
+) -> axum::response::Response {
+    templateInformation.into_response()
 }
